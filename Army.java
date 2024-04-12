2bb8ee809244784c12e3dc57b5993d2d697f0f9c
unknown <purushottamxworkz@gmail.com>
2024-04-12 20:27:54 +0530

    test

---
+++ Army.java
@@ -0,0 +1,12 @@
+class Army{
+	public static void main(String args[]){
+		int noOfMembers=1000;
+		String nameOfregiment="Sikh";
+		if(noOfMembers==10 || nameOfregiment=="Sikh"){
+			System.out.println("The value is true");
+		}
+		else{
+			System.out.println("The value is not true");
+		}
+	}
+}
