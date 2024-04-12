cdfd19ba5495c0d6c369b64ea4dd525b44e5822c
unknown <purushottamxworkz@gmail.com>
2024-04-12 23:15:14 +0530

    create 3 objects write main method SOP 10 lines

---
+++ WhiteBoard.java
@@ -0,0 +1,14 @@
+class WhiteBoard{
+	public static void main(String [] args){
+	System.out.println("A whiteboard also known by the terms marker board, dry-erase board, dry-wipe board, and pen-board.");
+	System.out.println("Whiteboards are analogous to blackboards, but with a smoother surface allowing for rapid marking and erasing of markings on their surface");
+	System.out.println("The term whiteboard is also used metaphorically in reference to features of computer software applications that simulate whiteboards.");
+	System.out.println("Whiteboards became commercially available in the early 1960s");
+	System.out.println("Whiteboards began being commonly used by businesses in the early 1990s.");
+	System.out.println("The first whiteboards were very expensive and were made of an enameled steel.");
+	System.out.println("Whiteboard material can be bought in rolls, sheets, and pre-formed boards. ");
+	System.out.println("There are six types of materials commonly used for whiteboard surfaces ");
+	System.out.println("Melamine,Steel or aluminum,Hardcoat laminate");
+	System.out.println("Porcelain, enamel-on-steel,Tempered glass and Polypropylene film");
+	}
+}
