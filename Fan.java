fec4699433fc708faf323614916e3accaea2c039
unknown <purushottamxworkz@gmail.com>
2024-04-12 23:12:05 +0530

    create 3 objects write main method SOP 10 lines

---
+++ Fan.java
@@ -0,0 +1,23 @@
+class Fan{
+	public static void main(String args[]){
+	System.out.println("A fan is a powered machine used to create a flow of air");
+	
+	System.out.println("A fan consists of a rotating arrangement of vanes or blades");
+	
+	System.out.println("The rotating assembly of blades and hub is known as an impeller");
+	
+	System.out.println("This may direct the airflow, or increase safety by preventing objects from contacting the fan blades");
+	
+	System.out.println("Most fans are powered by electric motors");
+	
+	System.out.println("Mechanically, a fan can be any revolving vane, or vanes used for producing currents of air");
+	
+	System.out.println("Fans produce air flows with high volume and low pressure,");
+	
+	System.out.println("As opposed to compressors which produce high pressures at a comparatively low volume.");
+	
+	System.out.println("A fan blade will often rotate when exposed to an air-fluid stream.");
+	
+	System.out.println("Typical applications include climate control and personal thermal comfort");
+	}
+}
