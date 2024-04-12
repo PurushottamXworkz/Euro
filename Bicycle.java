abbdc41446ee72a4271724182b60eb67c2262241
unknown <purushottamxworkz@gmail.com>
2024-04-12 23:08:34 +0530

    create 3 objects write main method SOP 10 lines

---
+++ Bicycle.java
@@ -0,0 +1,14 @@
+class Bicycle{
+	public static void main(String args[])	{
+	System.out.println("A bicycle, also called a pedal cycle.");
+	System.out.println("Bicycles were introduced in the 19th century in Europe.");
+	System.out.println("There are many more bicycles than cars.");
+	System.out.println("Bicycles are the principal means of transport in many regions.");
+	System.out.println("Bicycles are used for transportation, bicycle commuting, and utility cycling.");
+	System.out.println("Bicycles are also used professionally by mail carriers, paramedics, police, messengers, and general delivery services.");
+	System.out.println("Military uses of bicycles include communications, reconnaissance, troop movement, supply of provisions, and patrol, such as in bicycle infantries.");
+	System.out.println("The bicycle is an inexpensive, fast, healthy and environmentally friendly.");
+	System.out.println("Bicycle also has lowest carbon intensity of travel.");
+	System.out.println("Bicycles are also used for entertainment ");	
+	}
+}
