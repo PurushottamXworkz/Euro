267fbed9058ae597e418802ef8b2c4f166067534
unknown <purushottamxworkz@gmail.com>
2024-04-13 17:19:31 +0530

    Initilization and declartion of long and boolean

---
+++ Water.java
@@ -0,0 +1,27 @@
+class Water{
+	public static void main(String args[])
+	{
+	long meterNo;
+	meterNo=123456789987l;
+	boolean runningMeter;
+	runningMeter=true;
+	
+	long number;
+	number=123656789987l;
+	boolean notworkingMeter;
+	notworkingMeter=false;
+	
+	long No;
+	No=187456789987l;
+	boolean workingMeter;
+	workingMeter=false;
+	
+	
+	System.out.println("Meter Number: "+meterNo);
+	System.out.println("Running Meter: "+runningMeter);
+	System.out.println("Number: "+number);
+	System.out.println("Not-working Meter: "+notworkingMeter);
+	System.out.println("Number: "+No);
+	System.out.println("Working Meter: "+workingMeter);
+	}
+}
