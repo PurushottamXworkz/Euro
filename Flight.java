693a9c3f7b41b9704b3c93fc18a1a7c711d4e276
unknown <purushottamxworkz@gmail.com>
2024-04-13 17:49:31 +0530

    If conditional statement

---
+++ Flight.java
@@ -0,0 +1,43 @@
+class Flight{
+	public static void main(String args[]){
+		
+	 byte noOfFlight=124;
+	 short sizeOfFlight=38;
+	 int valueOfFlight=419;
+	 float lengthOfFlight=487.79f;
+	 double codeOfFlight=114528412;
+	 long contactOfFlight=8088445577l;
+	 boolean theFlightIsInTime=true;
+	 char seatSeries='M';
+	 String typeOfFlight="Domestic";
+		
+	
+	if(noOfFlight==124){
+			System.out.println("The number of Flight: "+noOfFlight);
+	}
+	if(sizeOfFlight==38){
+			System.out.println("The size of Flight: "+sizeOfFlight);
+	}
+	if(valueOfFlight==100){
+		System.out.println("The value of Flight: "+valueOfFlight);
+	}
+	if(lengthOfFlight==487.79f){
+		System.out.println("The length of Flight: "+lengthOfFlight);
+	}
+	if(codeOfFlight==1425467){
+		System.out.println("The code of Flight: "+codeOfFlight);
+	}
+	if(contactOfFlight==80884477l){
+		System.out.println("The contact of Flight: "+contactOfFlight);
+	}
+	if(theFlightIsInTime==false){
+		System.out.println("The flight is in time: "+theFlightIsInTime);
+	}
+	if(seatSeries=='M'){
+		System.out.println("The seat series of Flight: "+theFlightIsInTime);
+	}
+	if(typeOfFlight=="Domestic"){
+		System.out.println("The flight is domestic? : "+typeOfFlight);
+	}
+	}	
+}
