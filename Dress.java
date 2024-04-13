39e287044b21fd967f2a8d65c39bf19a21cf0425
unknown <purushottamxworkz@gmail.com>
2024-04-13 17:28:07 +0530

    local variable initialzation and declaration

---
+++ Dress.java
@@ -0,0 +1,23 @@
+class Dress{
+	public static void main(String args[]){
+	 byte noOfDress=124;
+	 short sizeOfDress=38;
+	 int valueOfDress=419;
+	 float valueOfDres=487.79f;
+	 double codeOfDress=114528412;
+	 long contactForOrder=8088445577l;
+	 boolean rightSize=true;
+	 char codeOfDres='M';
+	 String typeOfDress="Cotton";
+	 
+	 System.out.println("Number Of Dress: "+noOfDress);
+	 System.out.println("Size Of Dress: "+sizeOfDress);
+	 System.out.println("Value Of Dress: "+valueOfDress);
+	 System.out.println("Value Of Dres: "+valueOfDres);
+	 System.out.println("Code Of Dress: "+codeOfDress);
+	 System.out.println("Contact For Order: "+contactForOrder);
+	 System.out.println("Right Size: "+rightSize);
+	 System.out.println("Code Of Dress: "+codeOfDres);
+	 System.out.println("Type Of Dress: "+typeOfDress);
+	}
+}
