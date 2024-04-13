e2e9dd8cb4a80f6d1a792e0839c277f89bbec60f
unknown <purushottamxworkz@gmail.com>
2024-04-13 18:18:08 +0530

    Else If conditional statement

---
+++ Flowers.java
@@ -0,0 +1,80 @@
+class Flowers{
+	public static void main(String args[]){
+		
+		byte noOfFlowers=105;
+		short typesOfFlowers=890;
+		int valueOfFlowers=1846;
+		float lengthOfFlowers=875.124f;
+		double flowersInNumber=14528412;
+		long contactforFlowers=8088449720l;
+		boolean flowersAreFresh=false;
+		char typeOfFlowers='M';
+		String nameOfFlower="Mallige";
+		 
+		 
+		 
+		if(noOfFlowers<=40){
+			System.out.println("Number Of flowers: "+noOfFlowers);
+		}
+		else{
+			System.out.println("Number Of flowers are not valid");
+		}
+		
+		if(typesOfFlowers<=600){
+			System.out.println("Flowers types correct "+typesOfFlowers);
+		}
+		else{
+			System.out.println("Flowers types invalid: "+typesOfFlowers);
+		}
+		
+		
+		if(valueOfFlowers==2000){
+			System.out.println("Flowers value is valid: "+valueOfFlowers);
+		}
+		else{
+			System.out.println("Flowers value is invalid: "+valueOfFlowers);
+		}
+		
+		if(lengthOfFlowers<=50){
+			System.out.println("Flowers length is valid: "+lengthOfFlowers);
+		}
+		else{
+			System.out.println("Flowers length is invalid: "+lengthOfFlowers);
+		}
+		
+		if(flowersInNumber==148412){
+			System.out.println("Flowers number is valid: "+flowersInNumber);
+		}
+		else{
+			System.out.println("Flowers number is invalid: "+flowersInNumber);
+		}
+		
+		if(contactforFlowers==808888520l){
+			System.out.println("The contact is correct : "+contactforFlowers);
+		}
+		else{
+			System.out.println("The contact number is not valid : "+contactforFlowers);
+		}
+		
+		if(flowersAreFresh==true){
+			System.out.println("The flowers Are Fresh : "+flowersAreFresh);
+		}
+		else{
+			System.out.println("The flowers Are Fresh : "+flowersAreFresh);
+		}
+		
+		if(typeOfFlowers=='R'){
+			System.out.println("Type Of Flowers : "+typeOfFlowers);
+		}
+		else{
+			System.out.println("Type Of Flowers diffent : "+typeOfFlowers);
+		}
+		
+		if(nameOfFlower=="Mallige"){
+			System.out.println("Name Of flower is : "+nameOfFlower);
+		}
+		else{
+			System.out.println("Name Of flower is : "+nameOfFlower);
+		}
+	}
+}      
