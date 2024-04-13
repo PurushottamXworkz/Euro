6361bf96dbb8a738e352bbf2fbaa913f4e2dac47
unknown <purushottamxworkz@gmail.com>
2024-04-13 17:10:35 +0530

    Initialization and declaration of int , char

---
+++ Zomato.java
@@ -0,0 +1,25 @@
+class Zomato{
+	public static void main(String args[]){
+	int noOfOrders=15;
+	char orderName='A';
+	int noOfQuantity=3;
+	char orderFood='I';
+	int noOfDelevered=45;
+	char placeOfDelevery='R';
+	int billOrder=980;
+	char billSequence='Q';
+	int placeOfOrders=12;
+	char placeOfOrder='N';
+	
+	System.out.println("Nummber Of Orders: "+noOfOrders);
+	System.out.println("Order Name: "+orderName);
+	System.out.println("Number  Of Quantity: "+noOfQuantity);
+	System.out.println("Order of Food: "+orderFood);
+	System.out.println("Number Of Delevered: "+noOfDelevered);
+	System.out.println("Place Of Delevery: "+placeOfDelevery);
+	System.out.println("Bill Order: "+billOrder);
+	System.out.println("Bill Sequence: "+billSequence);
+	System.out.println("Place Of Orders: "+placeOfOrders);
+	System.out.println("Place Of Order: "+placeOfOrder);
+	}
+}
