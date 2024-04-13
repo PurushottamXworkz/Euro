a8b47f5e1234c414c3f203b2125db0c554e84e05
unknown <purushottamxworkz@gmail.com>
2024-04-13 17:26:41 +0530

    Static variable declaration and initialzation

---
+++ Train.java
@@ -0,0 +1,22 @@
+class Train{
+	static byte noOfTrains=10;
+	static short valueOfTrain=201;
+	static int noOfSeatsAvailable=276;
+	static float valueOfFood=84.79f;
+	static double sizeOfTrain=1415.456741254896321;
+	static long contactForBooking=8088445577l;
+	static boolean allTrainsInTime=true;
+	static char typeOfCoach='S';
+	static String nameOfTrain="Vande Bharat";
+	public static void main(String args[]){
+		System.out.println("Number Of Trains: "+noOfTrains);
+		System.out.println("Value Of Train: "+valueOfTrain);
+		System.out.println("Number Of Seats Available: "+noOfSeatsAvailable);
+		System.out.println("Value Of Food: "+valueOfFood);
+		System.out.println("Size Of Train: "+sizeOfTrain);
+		System.out.println("Contact For Booking: "+contactForBooking);
+		System.out.println("All Trains In Time: "+allTrainsInTime);
+		System.out.println("Type Of Coach: "+typeOfCoach);
+		System.out.println("Name Of Train: "+nameOfTrain);
+	}
+}
