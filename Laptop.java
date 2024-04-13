3b3470a6a17a13d4c7d245ad355398a6eba12807
unknown <purushottamxworkz@gmail.com>
2024-04-13 17:29:36 +0530

    Local variable initialization and declartion

---
+++ Laptop.java
@@ -0,0 +1,22 @@
+class Laptop{
+	public static void main(String args[]){
+	byte noOfLaptops=124;
+	 short sizeOfLaptop=38;
+	 int valueOfLaptop=419;
+	 float valueOfLaptops=487.79f;
+	 double codeOfLaptop=114528412;
+	 long contactForOrder=8088445577l;
+	 boolean workingLaptop=true;
+	 char coolorOfLaptop='M';
+	 String typeOfLaptop="Gaming laptop";
+	 
+	 System.out.println("Number Of Laptops: "+noOfLaptops);
+	 System.out.println("Size Of Laptop: "+sizeOfLaptop);
+	 System.out.println("Value Of Laptop: "+valueOfLaptop);
+	 System.out.println("Code Of Laptop: "+codeOfLaptop);
+	 System.out.println("Contact For Order: "+contactForOrder);
+	 System.out.println("Working Laptop: "+workingLaptop);
+	 System.out.println("Coolor Of Laptop: "+coolorOfLaptop);
+	 System.out.println("Type Of Laptop: "+typeOfLaptop);
+	}
+}
