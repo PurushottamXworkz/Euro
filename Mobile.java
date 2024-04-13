041fb928bb145b127f6385f65d900cea44bf0b7a
unknown <purushottamxworkz@gmail.com>
2024-04-13 17:50:14 +0530

    If conditional statement

---
+++ Mobile.java
@@ -0,0 +1,43 @@
+class Mobile{
+	public static void main(String args[]){
+		 
+		 byte noOfMobiles=74;
+		 short sizeOfMobiles=24;
+		 int valueOfMobile=419;
+		 float lengthOfMobile=487.79f;
+		 double codeOfMobile=114528412;
+		 long contactOfMobile=8088445577l;
+		 boolean theMobileIsWorking=true;
+		 char mobileSeries='R';
+		 String typeOfMobile="Regular";
+	 
+	 
+		if(noOfMobiles==180){
+			System.out.println("Number of Mobile are: "+noOfMobiles);
+		}
+		if(sizeOfMobiles==25){
+		System.out.println("Size of Mobiles : "+sizeOfMobiles);	
+		}
+		if(valueOfMobile<480){
+		System.out.println("Value of Mobile: "+valueOfMobile);
+		}
+		if(lengthOfMobile<=490){
+		System.out.println("Length Of Mobile: "+lengthOfMobile);	
+		}
+		if(codeOfMobile==114528412){
+		System.out.println("Code Of Mobile: "+codeOfMobile);	
+		}
+		if(contactOfMobile==8088204455l){
+			System.out.println("Contact Of Mobile: "+contactOfMobile);
+		}
+		if(theMobileIsWorking==false){
+		System.out.println("The Mobile is Working: "+theMobileIsWorking);	
+		}
+		if(mobileSeries=='R'){
+		System.out.println("Mobile Series : "+mobileSeries);	
+		}
+		if(typeOfMobile=="Regular"){
+		System.out.println("Type Of Mobile"+typeOfMobile);	
+		}
+	}
+}
