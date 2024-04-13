30670db3124de578f7c1fafb4c3a16f7fc200170
unknown <purushottamxworkz@gmail.com>
2024-04-13 17:17:54 +0530

    Initialization and declaration of long and boolean

---
+++ Bulb.java
@@ -0,0 +1,25 @@
+class Bulb{
+	public static void main(String args[]){
+	long serialNumber;
+	serialNumber=8745124529524215l;
+	boolean working;
+	working=true;
+	
+	long sNumber;
+	sNumber=8745124529524285l;
+	boolean turningOff;
+	turningOff=false;
+	
+	long sequenceNumber;
+	sequenceNumber=874512452952494l;
+	boolean turningOn;
+	turningOn=false;
+	
+	System.out.println("Serial Number: "+serialNumber);
+	System.out.println("Working: "+working);
+	System.out.println("sNumber: "+sNumber);
+	System.out.println("turningOff: "+turningOff);
+	System.out.println("Sequence Number: "+sequenceNumber);
+	System.out.println("Turning On: "+turningOn);
+	}
+}
