3f0465aafabb16bc0f7eb21361dd5edc6077d3b8
unknown <purushottamxworkz@gmail.com>
2024-04-13 17:24:04 +0530

    static variable example

---
+++ Pen.java
@@ -0,0 +1,21 @@
+class Pen{
+	static byte noOfPenBoxes=15;
+	static short valueOfPen=50;
+	static int noOfPens=1000;
+	static float boxValueOfPens=55.74f;
+	static double sizeOfPen=1.456741254896321;
+	static long contactForPens=7454879541l;
+	static boolean workingAllPens=true;
+	static char typeOfPen='b';
+	static String brandName="Orimo";
+	public static void main(String args[]){
+		System.out.println("Number Of PenBoxes: "+noOfPenBoxes);
+		System.out.println("Value Of Pen: "+valueOfPen);
+		System.out.println("Box Value Of Pens: "+boxValueOfPens);
+		System.out.println("Size Of Pen: "+sizeOfPen);
+		System.out.println("Contact For Pens: "+contactForPens);
+		System.out.println("Working All Pens: "+workingAllPens);
+		System.out.println("Type Of Pen: "+typeOfPen);
+		System.out.println("Brand Name: "+brandName);
+	}
+}
