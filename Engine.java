1f1dfb688035930e173604592ed354b466d6f3aa
unknown <purushottamxworkz@gmail.com>
2024-04-13 18:22:21 +0530

    For loop statement

---
+++ Engine.java
@@ -0,0 +1,21 @@
+class Engine{
+		
+		public static void main(String args[]){
+		int engine=1;
+		byte engineNumber=1;
+		short engineVehicle=1;
+		long engineSize=89;
+		for(int i=1;i<=engine;i++){
+			System.out.println("engine: "+i);
+		}
+		for(int i=0;i<engineNumber;i++){
+			System.out.println("engineNumber: "+i);
+		}
+		for(int i=0;i<engineVehicle;i++){
+			System.out.println("engineVehicle: "+engineVehicle);
+		}
+		for(int i=0;i<engineSize;i++){
+			System.out.println("engineSize "+i);
+		}
+		}
+}
