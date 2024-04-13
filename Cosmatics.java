8b8174718f56188e2b17e40f1a23a3b9a3b4dff1
unknown <purushottamxworkz@gmail.com>
2024-04-13 17:12:25 +0530

    Initialization and declaration of int, char

---
+++ Cosmatics.java
@@ -0,0 +1,25 @@
+class Cosmatics{
+	public static void main(String args[]){
+	int noOfCos=15;
+	char nameOfCos='A';
+	int costOfCos=139;
+	char serialNameOfCos='A';
+	int cosWeight=150;
+	char cosBillNo='B';
+	int cosDiscount=25;
+	char cosColour='G';
+	int cosNetWeight=25;
+	char cosSize='L';
+	
+	System.out.println("Number Of Cos: "+noOfCos);
+	System.out.println("Name Of Cos: "+nameOfCos);
+	System.out.println("Cost Of Cos: "+costOfCos);
+	System.out.println("Serial Name Of Cos: "+serialNameOfCos);
+	System.out.println("Cos Weight: "+cosWeight);
+	System.out.println("Cos Bill No: "+cosBillNo);
+	System.out.println("Cos Discount: "+cosDiscount);
+	System.out.println("Cos Colour: "+cosColour);
+	System.out.println("Cos Net Weight: "+cosNetWeight);
+	System.out.println("Cos Size: "+cosSize);
+	}
+}
