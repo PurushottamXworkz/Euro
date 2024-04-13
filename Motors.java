349833523f9e29630be798322ee7e2acfed8010d
unknown <purushottamxworkz@gmail.com>
2024-04-13 18:20:29 +0530

    For loop example

---
+++ Motors.java
@@ -0,0 +1,23 @@
+class Motors{
+	public static void main(String args[]){
+	
+		int motors=1;
+		byte motorNumber=1;
+		short motorVehicle=1;
+		long motorSize=10l;
+		for(int i=1;i<=motors;i++){
+			System.out.println("motors: "+i);
+		}
+		for(int i=0;i<motorNumber;i++){
+			System.out.println("Motor Number: "+i );
+		}
+		for(int i=0;i<motorVehicle;i++){
+			System.out.println("motorVehicle: "+motorVehicle);
+		}
+		for(int i=0;i<motorSize;i++){
+			System.out.println("motorSize "+i);
+		}
+		
+		
+	}
+}
