8bdf21740ff85c7fcdf6aa0ec85c572e32fccb3c
unknown <purushottamxworkz@gmail.com>
2024-04-13 18:26:17 +0530

    For loop statement

---
+++ WindMill.java
@@ -0,0 +1,20 @@
+class WindMill{
+	public static void main(String args[]){
+		int WindMill=1;
+		byte windMillNumber=1;
+		short windMillseries=7;
+		long windMillSize=20;
+		for(int i=1;i<=WindMill;i++){
+			System.out.println("WindMill: "+i);
+		}
+		for(int i=0;i<windMillNumber;i++){
+			System.out.println("windMillNumber: "+i);
+		}
+		for(int i=0;i<windMillseries;i++){
+			System.out.println("windMillseries: "+windMillseries);
+		}
+		for(int i=0;i<=windMillSize;i++){
+			System.out.println("windMillSize "+i);
+		}
+		}
+}
