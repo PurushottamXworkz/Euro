ba7aeb96f82a11c8428cf0803552e0f77b90dc43
unknown <purushottamxworkz@gmail.com>
2024-04-13 18:16:08 +0530

    Else If condiational statement

---
+++ Buttons.java
@@ -0,0 +1,80 @@
+class Buttons{
+	public static void main(String args[]){
+		
+		byte noOfButtons=74;
+		short sizeOfButtons=600;
+		int valueOfButtons=1846;
+		float lengthOfButtons=75.124f;
+		double buttonsInNumber=14528412;
+		long contactforButtons=8088449720l;
+		boolean theButtonIsOpen=false;
+		char typeOfButtons='L';
+		String nameOfButtons="AmericanPan";
+		 
+		 
+		 
+		if(noOfButtons<=40){
+			System.out.println("Number Of Buttons: "+noOfButtons);
+		}
+		else{
+			System.out.println("Number Of Buttons are not valid");
+		}
+		
+		if(sizeOfButtons<=600){
+			System.out.println("Buttons size is correct "+sizeOfButtons);
+		}
+		else{
+			System.out.println("Buttons size is invalid: "+sizeOfButtons);
+		}
+		
+		
+		if(valueOfButtons==2000){
+			System.out.println("Buttons value is valid: "+valueOfButtons);
+		}
+		else{
+			System.out.println("Buttons value is invalid: "+valueOfButtons);
+		}
+		
+		if(lengthOfButtons<=50){
+			System.out.println("Buttons length is valid: "+lengthOfButtons);
+		}
+		else{
+			System.out.println("Buttons length is invalid: "+lengthOfButtons);
+		}
+		
+		if(buttonsInNumber==148412){
+			System.out.println("Buttons number is valid: "+buttonsInNumber);
+		}
+		else{
+			System.out.println("Buttons number is invalid: "+buttonsInNumber);
+		}
+		
+		if(contactforButtons==808888520l){
+			System.out.println("The mobile contact is correct : "+contactforButtons);
+		}
+		else{
+			System.out.println("The contact number is not valid : "+contactforButtons);
+		}
+		
+		if(theButtonIsOpen==true){
+			System.out.println("The Button is Open : "+theButtonIsOpen);
+		}
+		else{
+			System.out.println("The Button is closed : "+theButtonIsOpen);
+		}
+		
+		if(typeOfButtons=='R'){
+			System.out.println("Register Name Of Button : "+typeOfButtons);
+		}
+		else{
+			System.out.println("Register Name Of Button is diffent : "+typeOfButtons);
+		}
+		
+		if(nameOfButtons=="AmericanPan"){
+			System.out.println("Name Of Shop is : "+nameOfButtons);
+		}
+		else{
+			System.out.println("Name Of Shop is : "+nameOfButtons);
+		}
+	}
+}
