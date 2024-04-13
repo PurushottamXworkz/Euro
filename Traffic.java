50d238672714e9d63271407b694765712a3be400
unknown <purushottamxworkz@gmail.com>
2024-04-13 17:21:15 +0530

    Initialzation and declartion of long and boolean in same line

---
+++ Traffic.java
@@ -0,0 +1,26 @@
+class Traffic{
+	public static void main(String args[]){
+	long noTrafficSignals=1452365421563l;
+	boolean workingSignals=true;
+	
+	long trafficSignals=1452745421563l;
+	boolean signal=true;
+	
+	long signals=1452365421564l;
+	boolean notWorkingSignals=false;
+	
+	
+	System.out.println("Number of Traffic Signals: "+noTrafficSignals);
+	
+	System.out.println("Working Signals: "+workingSignals);
+	
+	System.out.println("Traffic Signals: "+trafficSignals);
+	
+	System.out.println("Signal: "+signal);
+	
+	System.out.println("signals: "+signals);
+	
+	System.out.println("Not Working Signals: "+notWorkingSignals);
+	
+	}
+}
