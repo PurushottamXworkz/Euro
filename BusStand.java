532b57cc1adca79576fdf7320955b3b3c84b60f4
unknown <purushottamxworkz@gmail.com>
2024-04-13 17:02:41 +0530

    Initialization and declaration of int, char

---
+++ BusStand.java
@@ -0,0 +1,46 @@
+class BusStand{
+	public static void main(String args[]){
+	
+	int staffInBusStand;
+	staffInBusStand=128;
+	
+	char nameOfBusStand;
+	nameOfBusStand='B';
+	
+	int areaOfBusStand;
+	areaOfBusStand=400;
+	
+	char locationOfbusStand;
+	locationOfbusStand='U';
+	
+	int noOfBuses;
+	noOfBuses=50;
+	
+	char busDivision;
+	busDivision='E';
+	
+	int typeOfBusses;
+	typeOfBusses=4;
+	
+	char routeOfBus;
+	routeOfBus='E';
+	
+	int engineOfBus;
+	engineOfBus=2;
+	
+	char nameOfEngine;
+	nameOfEngine='R';
+	
+	
+	System.out.println("Staff In Bus Stand : "+staffInBusStand);
+	System.out.println("Name Of Bus Stand: "+nameOfBusStand);
+	System.out.println("Area Of Bus Stand : "+areaOfBusStand);
+	System.out.println("Location Of bus Stand"+locationOfbusStand);
+	System.out.println("Number Of Buses : "+noOfBuses);
+	System.out.println("Bus Division: "+busDivision);
+	System.out.println("Type Of Busses : "+typeOfBusses);
+	System.out.println("Route Of Bus : "+routeOfBus);
+	System.out.println("Engine Of Bus : "+engineOfBus);
+	System.out.println("Name Of Engine : "+nameOfEngine);
+	}
+}
