fd7b1af046f2e6a490914af7c51a4a4739fa8b56
unknown <purushottamxworkz@gmail.com>
2024-04-13 17:00:00 +0530

    Initialisation and declaration of int, char

---
+++ Bike.java
@@ -0,0 +1,45 @@
+class Bike{
+	public static void main(String args[]){
+	
+	int numberOfGares;
+	numberOfGares=5;
+	
+	char nameOfBike;
+	nameOfBike='A';
+	
+	int numberOfStrokes;
+	numberOfStrokes=2;
+	
+	char nameOfEngine;
+	nameOfEngine='M';
+	
+	int numberOfBreakes;
+	numberOfBreakes=2;
+	
+	char nameOfBrake;
+	nameOfBrake='F';
+	
+	int numberOfClutchPlates;
+	numberOfClutchPlates=12;
+	
+	char nameOfClutch;
+	nameOfClutch='C';
+	
+	int capacityOfFuel;
+	capacityOfFuel=10;
+	
+	char typeOfFuel;
+	typeOfFuel='P';
+	
+	System.out.println("Name Of Bike : "+nameOfBike);
+	System.out.println("Number Of Gare : "+numberOfGares);
+	System.out.println("Number Of Strokes : "+numberOfStrokes);
+	System.out.println("Name Of Engine : "+nameOfEngine);
+	System.out.println("Number Of Breakes : "+numberOfBreakes);
+	System.out.println("Name Of Brake : "+nameOfBrake);
+	System.out.println("Number Of Clutch Plates : "+numberOfClutchPlates);
+	System.out.println("Name Of Clutch : "+nameOfClutch);
+	System.out.println("Capacity Of Fuel : "+capacityOfFuel);
+	System.out.println("Type Of Fuel : "+typeOfFuel);
+	}
+}
