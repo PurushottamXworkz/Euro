d597721b1397a46b96a63f0c30e906598ae5497b
unknown <purushottamxworkz@gmail.com>
2024-04-13 17:15:02 +0530

    Initialization and declaration of int, char

---
+++ Medicals.java
@@ -0,0 +1,26 @@
+class Medicals{
+		public static void main(String args[])
+		{
+		int noOfMedicines=1024;
+		char nameOfMedicine='A';
+		int typesOfMedicine=23;
+		char nameOfdiseaseA='P';
+		int contentOfMedicines=12;
+		char contentName='Q';
+		int noOfOrders=14;
+		char nameOfOrder='O';
+		int quantityOfMedicines=15;
+		char nameOfStore='N';
+		
+		System.out.println("number Of Medicines: "+noOfMedicines);
+		System.out.println("Name Of Medicine: "+nameOfMedicine);
+		System.out.println("Types Of Medicine: "+typesOfMedicine);
+		System.out.println("Name Of DiseaseA: "+nameOfdiseaseA);
+		System.out.println("Content Of Medicines: "+contentOfMedicines);
+		System.out.println("Number Of Orders: "+noOfOrders);
+		System.out.println("Name Of Order: "+nameOfOrder);
+		System.out.println("Quantity Of Medicines: "+quantityOfMedicines);
+		System.out.println("Name Of Store: "+nameOfStore);
+		System.out.println("Content Name: "+contentName);	
+		}
+}
