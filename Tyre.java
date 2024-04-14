f11565bf3540ebbb7cf197a2766ded28153f9d2f
unknown <purushottamxworkz@gmail.com>
2024-04-14 12:04:08 +0530

    Example for AND operator

---
+++ Tyre.java
@@ -0,0 +1,12 @@
+class Tyre{
+	public static void main(String args[]){
+		int noOfTyre=10;
+		String nameOfVehicle="Lorry";
+		if(noOfTyre==10 && nameOfVehicle=="Lory"){
+			System.out.println("The value is true");
+		}
+		else{
+			System.out.println("The value is not true");
+		}
+	}
+}
