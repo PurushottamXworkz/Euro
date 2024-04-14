eb85fbbc9859a10881fb60e86f1e0a1151fa2e8c
unknown <purushottamxworkz@gmail.com>
2024-04-14 12:02:18 +0530

    Working of Operator AND

---
+++ Temple.java
@@ -0,0 +1,12 @@
+class Temple{
+	public static void main(String args[]){
+		int noOfTemples=10;
+		String nameOfTemple="Ram-Mandir";
+		if(noOfTemples==10 && nameOfTemple=="Ram-Mandir"){
+			System.out.println("The value is true");
+		}
+		else{
+			System.out.println("The value is not true");
+		}
+	}
+}
