4bb8ad05f6d056dbf9b8183ee5cf771d5d90d5f4
unknown <purushottamxworkz@gmail.com>
2024-04-14 12:05:36 +0530

    Example for OR operator

---
+++ Wire.java
@@ -0,0 +1,12 @@
+class Wire{
+	public static void main(String args[]){
+		int noOfWire=84;
+		String nameOfWire="VGaurd";
+		if(noOfWire==10 || nameOfWire=="VGaurd"){
+			System.out.println("The value is true");
+		}
+		else{
+			System.out.println("The value is not true");
+		}
+	}
+}
