79ca3cba94d834a6a0fb5d761699bc98121fa8a0
unknown <purushottamxworkz@gmail.com>
2024-04-14 11:59:44 +0530

    Conditional Switch case example

---
+++ Plate.java
@@ -0,0 +1,52 @@
+class Plate{
+	public static void main(String args[]){
+		int plateNumber=10;
+		
+		switch(plateNumber){
+			case 1: 
+			System.out.println("plateNumber:1");
+			break;
+			
+			case 2:
+			System.out.println("plateNumber: 2");
+			break;
+			
+			case 3:
+			System.out.println("plateNumber: 3");
+			break;
+			
+			case 4:
+			System.out.println("plateNumber: 4");
+			break;
+			
+			case 5:
+			System.out.println("plateNumber: 5");
+			break;
+			
+			case 6:
+			System.out.println("plateNumber: 6");
+			break;
+			
+			case 7:
+			System.out.println("plateNumber: 7");
+			break;
+			
+			case 8:
+			System.out.println("plateNumber: 8");
+			break;
+			
+			case 9:
+			System.out.println("pltaeNumber: 9");
+			break;
+			
+			case 10:
+			System.out.println("plateNumber: 10");
+			break;
+			
+			default:
+			System.out.println("None of the above");
+			break;
+			
+		}
+	}
+}
