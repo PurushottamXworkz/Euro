f06aa48680559925c25a8f61aca4ecb14fa8b17b
unknown <purushottamxworkz@gmail.com>
2024-04-14 12:00:47 +0530

    Conditional Switch case statement example

---
+++ Blade.java
@@ -0,0 +1,51 @@
+class Blade{
+	public static void main(String args[]){
+		int blades=10;
+		
+		switch(blades){
+			case 1: 
+			System.out.println("blades:1");
+			break;
+			
+			case 2:
+			System.out.println("blades: 2");
+			break;
+			
+			case 3:
+			System.out.println("blades: 3");
+			break;
+			
+			case 4:
+			System.out.println("blades: 4");
+			break;
+			
+			case 5:
+			System.out.println("blades: 5");
+			break;
+			
+			case 6:
+			System.out.println("blades: 6");
+			break;
+			
+			case 7:
+			System.out.println("blades: 7");
+			break;
+			
+			case 8:
+			System.out.println("blades: 8");
+			break;
+			
+			case 9:
+			System.out.println("blades: 9");
+			break;
+			
+			case 10:
+			System.out.println("blades: 10");
+			break;
+			
+			default:
+			System.out.println("None of the above");
+			break;
+		}
+	}
+}
