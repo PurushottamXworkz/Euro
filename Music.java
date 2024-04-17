04e392ffefdcfdd4c89f9a9a7ff2bb4f4bc9e7ee
unknown <prashantpattanashetti5@gmail.com>
2024-04-17 11:16:17 +0530

    States and Methods in java

---
+++ Music.java
@@ -0,0 +1,11 @@
+class Music{
+	static String nameOfIns="Guitar";
+	public static void infoOfMusic(){
+		System.out.println("Name Of Music instrument :"+nameOfIns);
+	} 
+	public static void noOfVeins(){
+		int numberOfVeins=7;
+		infoOfMusic();
+		System.out.println("Number Of Veins of Guitar : "+numberOfVeins);
+	}
+}
