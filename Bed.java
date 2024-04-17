e1b2ada1576e4be64e0b759da722611194a98bd5
unknown <prashantpattanashetti5@gmail.com>
2024-04-17 10:59:39 +0530

    States and behaviour of java

---
+++ Bed.java
@@ -0,0 +1,13 @@
+class Bed{
+	static int noOfBeds=4;
+	public static void detailsOfBed(){
+		System.out.println("Number of Beds: "+noOfBeds);
+		nameOfOwner();
+	}
+	
+	public static void nameOfOwner(){
+		String bedOwner="Raj";
+		System.out.println("Name Of Owner"+bedOwner);
+		
+	}
+}
