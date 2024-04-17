470e89de6d0feaff5d5583cf1d003e1d5adf81ae
unknown <prashantpattanashetti5@gmail.com>
2024-04-17 11:10:06 +0530

    States and Methods in java

---
+++ EarPod.java
@@ -0,0 +1,12 @@
+class EarPod{
+	static String typeOfEarPod="Bluetooth";
+	public static void infoOfearPod(){
+		System.out.println("Type Of EarPod "+typeOfEarPod);
+		
+	}
+	public static void deatailsOfearPod(){
+		String nameOfEarPod="Zebronics";
+		System.out.println("Name Of EarPod : "+nameOfEarPod);
+		infoOfearPod();
+	}
+}
