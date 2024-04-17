a2f139361dac4c3101b719d2c0dcffb5eabcd5de
unknown <prashantpattanashetti5@gmail.com>
2024-04-17 11:17:49 +0530

    States and methods in java

---
+++ Window.java
@@ -0,0 +1,11 @@
+class Window{
+	static int noOfframes=8;
+	public static void framesOfWindow(){
+		System.out.println("Number of frames : "+noOfframes);
+		locationOfWindow();
+	}
+	public static void locationOfWindow(){
+		String locOfWindow="Door side";
+		System.out.println("location Of Window : "+locOfWindow);
+	}
+}
