fcf14ae6233d05cca1cf03404fc57698c59efe53
unknown <prashantpattanashetti5@gmail.com>
2024-04-17 11:17:01 +0530

    States and Methods in java

---
+++ MusicRunner.java
@@ -0,0 +1,7 @@
+class MusicRunner{
+	public static void main(String args[]){
+		System.out.println("Name Of Music instrument :"+Music.nameOfIns);
+		Music.infoOfMusic();
+		Music.noOfVeins();
+	}
+}
