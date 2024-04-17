0597d1f3d9c953366a9c4038bc32cdaffe1d6b56
unknown <prashantpattanashetti5@gmail.com>
2024-04-17 11:13:31 +0530

    States and methods in Java

---
+++ Hotel.java
@@ -0,0 +1,16 @@
+class Hotel{
+	static int noOfHotels=100;
+	
+	public static void hotelName(){
+		String nameOfHotel="Sagar";
+		String locOfHotel="Dharwad";
+		System.out.println("Name of Hotel: "+nameOfHotel);
+		System.out.println("Location of hotel: "+locOfHotel);
+	}
+	public static void hotelDetails(){
+		System.out.println("Number Of Hotels "+noOfHotels);
+		hotelName();
+		
+	}
+	
+}
