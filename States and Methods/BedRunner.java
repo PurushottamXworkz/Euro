788515e7ffbe3b88b1cb1785924c79cdc0de5f09
Purushottam P <166138384+PurushottamXworkz@users.noreply.github.com>
2024-04-17 11:30:08 +0530

    Delete States and Methods directory

--- States and Methods/BedRunner.java
+++
@@ -1,7 +0,0 @@
-class BedRunner{
-	public static void main(String args[]){
-		System.out.println("Information from Bed");
-		Bed.getDetails();
-		System.out.println("Number of Beds : "+Bed.noOfBeds);
-	}
-}
