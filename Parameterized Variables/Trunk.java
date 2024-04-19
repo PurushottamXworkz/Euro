be40806a7d8e9297602367d69749a3c4558f91fe
unknown <Prashantpattanashetti5@gmail.com>
2024-04-19 10:46:57 +0530

    Test folder

---
+++ Parameterized Variables/Trunk.java
@@ -0,0 +1,43 @@
+class Trunk{
+	public static void thickNessOfTrunk(int thickNess){
+		System.out.println("Thick Ness Of Trunk : "+thickNess);
+	}
+	
+	public static void nOfTrunks(byte nOfT){
+		System.out.println("Number of Trunks in short : "+nOfT);
+	}
+	
+	public static void noOfTrunks(short noOfT){
+		System.out.println("Number of Trunks in int : "+noOfT);
+	}
+	
+	public static void mrpOfTrunk(float mrp){
+		System.out.println("MRP Of Trunk : "+mrp);
+	}
+	
+	public static void contactForTrunk(long contact){
+		System.out.println("Contact for Trunk : "+contact);
+	}
+
+	public static void valueOfTrunk(double value){
+		System.out.println("value of Trunk : "+value);
+	}
+	
+	public static void seriesOfTrunk(char series){
+		System.out.println("Series of Trunk : "+series);
+	}
+	
+	public static void availabilityOfTrunk(boolean avail){
+		System.out.println("Availability Of Trunk : "+avail);
+	}
+
+	public static void nameOfTrunks(String nameOfT){
+		System.out.println("Name of Trunks in String : "+nameOfT);
+	}
+	
+	
+
+	
+	
+	
+}
---
+++ Parameterized Variables/TrunkRunner.java
@@ -0,0 +1,13 @@
+class TrunkRunner{
+	public static void main(){
+		Trunk.noOfTrunks(10);
+		Trunk.nameOfTrunks("Pinkpop");
+		Trunk.numberOfTrunks(45);
+		Trunk.thickNessOfTrunk(8);
+		Trunk.contactForTrunk(8741246574l);
+		Trunk.valueOfTrunk(8456741.2231456789);
+		Trunk.seriesOfTrunk('F');
+		Trunk.mrpOfTrunk(99.99f);
+		Trunk.availabilityOfTrunk(true);
+	}
+}
