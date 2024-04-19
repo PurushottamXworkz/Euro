3efb76ce1bab39bedd532c7ffbedb5d94eac9676
unknown <Prashantpattanashetti5@gmail.com>
2024-04-19 09:05:18 +0530

    This is Ftb class

---
+++ StatesAndMethods/Ftb.java
@@ -0,0 +1,9 @@
+class FtbRunner{
+	public static void main(String args[]){
+		System.out.println("This is main method of FTB");
+		Flute.infoOfFlute();
+		Trunk.typeOfTrunk();
+		Brush.detailsOfBrush();
+		System.out.println("This is end of FTB");
+	}
+}
