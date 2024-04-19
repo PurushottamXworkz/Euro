c775bcfe02fd575f4d79beeafaa20e8a3ee5e67e
unknown <Prashantpattanashetti5@gmail.com>
2024-04-19 12:03:26 +0530

    BrushRunner class

---
+++ Parameterized Variables/BrushRunner.java
@@ -0,0 +1,13 @@
+class BrushRunner{
+	public static void main(String args[]){
+		//Brush.sizeOfBrush(7);
+		//Brush.valueOfBrush(10);
+		Brush.numberOfBrush(240);
+		Brush.mrpOfBrush(32.99f);
+		Brush.contactForBrush(8088561144l);
+		Brush.valuesOfBrush(8765.9874112d);
+		Brush.seriesOfBrush('A');
+		Brush.availabilityOfBrush(true);
+		Brush.nameOfBrush("Collegate");
+	}
+}
