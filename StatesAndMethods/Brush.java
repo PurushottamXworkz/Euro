6c765fd9dd012189a3b4b0b0158a54ad2b815205
unknown <Prashantpattanashetti5@gmail.com>
2024-04-19 08:59:29 +0530

    This is Brush class

---
+++ StatesAndMethods/Brush.java
@@ -0,0 +1,8 @@
+class Brush{
+	public static void detailsOfBrush(){
+		int noOfBrush=45;
+		System.out.println("This is detailsOfBrush method");
+		Trunk.typeOfTrunk();
+		System.out.println("End of detailsOfBrush method");
+	}
+}
