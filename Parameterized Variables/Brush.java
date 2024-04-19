8077c1dd426170de6d52a4ccd71cbcc149e66091
unknown <Prashantpattanashetti5@gmail.com>
2024-04-19 12:02:25 +0530

    Brush class

---
+++ Parameterized Variables/Brush.java
@@ -0,0 +1,37 @@
+class Brush{
+	public static void sizeOfBrush(byte x){
+		System.out.println("Size of Brush: "+x);
+	}
+	
+	public static void valueOfBrush(short value){
+		System.out.println("Number of Brush in int : "+value);
+	}
+	
+	public static void numberOfBrush(int brus){
+		System.out.println("Number Of Brush : "+brus);
+	} 
+	
+	public static void mrpOfBrush(float mrp){
+		System.out.println("MRP Of Brush : "+mrp);
+	}
+	
+	public static void contactForBrush(long contact){
+		System.out.println("Contact for Brush : "+contact);
+	}
+
+	public static void valuesOfBrush(double value){
+		System.out.println("value of Brush: "+value);
+	}
+	
+	public static void seriesOfBrush(char series){
+		System.out.println("Series of Brush : "+series);
+	}
+	
+	public static void availabilityOfBrush(boolean avail){
+		System.out.println("Availability Of Brush : "+avail);
+	}
+
+	public static void nameOfBrush(String nameOfBrus){
+		System.out.println("Name of Trunks in String : "+nameOfBrus);
+	}
+}
