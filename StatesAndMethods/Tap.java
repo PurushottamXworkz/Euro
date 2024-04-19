6a155b62e610bede31c228490d550ab0ed5dedf9
unknown <Prashantpattanashetti5@gmail.com>
2024-04-19 09:07:54 +0530

    This is tap class

---
+++ StatesAndMethods/Tap.java
@@ -0,0 +1,9 @@
+class Tap{
+	public static void noOfTaps(){
+		String nameOfTap="WaterTack";
+		System.out.println("Number of taps method name of Tap is : "+nameOfTap);
+		Flute.infoOfFlute();
+		Gun.gunDetails();
+		System.out.println("This is Tap class noOfTaps method");
+	}
+}
