25ae7759508d786251e55b35ecaf29e2eae767ec
unknown <Prashantpattanashetti5@gmail.com>
2024-04-19 09:01:24 +0530

    this is Flute class

---
+++ StatesAndMethods/Flute.java
@@ -0,0 +1,10 @@
+class Flute{
+	public static void infoOfFlute(){
+		int noOfFlutes=21;
+		System.out.println("This is info Of Flute method");
+		Gun.gunsInNumber();
+		Gun.gunDetails();
+		System.out.println("Number of flutes in infoOfFlute method: "+noOfFlutes);
+	}
+	
+}
