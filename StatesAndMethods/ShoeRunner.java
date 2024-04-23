5d40d00865325b4dbe79160817580b73936d6a3e
unknown <purushottamxworkz@gmail.com>
2024-04-23 13:38:43 +0530

    Updated polymorphism

--- StatesAndMethods/ShoeRunner.java
+++ StatesAndMethods/ShoeRunner.java
@@ -2,6 +2,10 @@ class ShoeRunner{
 	public static void main(String args[]){
 		Shoe.lengthOfShoe(24);
 		Shoe.lengthOfShoe(24,121.321f);
-		Shoe.lengthOfShoe(24,'L');
+		Shoe.lengthOfShoe(24,121.321f,'L');
+		Shoe.lengthOfShoe(24,121.321f,'L',8574947768l);
+		Shoe.lengthOfShoe(24,121.321f,'L',8574947768l,9874.3256478515);
+		Shoe.lengthOfShoe(24,121.321f,'L',8574947768l,9874.3256478515,"Adidas");
+		Shoe.lengthOfShoe(24,121.321f,'L',8574947768l,9874.3256478515,"Adidas",true);
 	}
 }
