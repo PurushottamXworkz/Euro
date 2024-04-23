738c0d95c189f5e944238141d3b96d290a41f0d1
unknown <purushottamxworkz@gmail.com>
2024-04-23 14:34:36 +0530

    Newly added file

---
+++ StatesAndMethods/Ballon.java
@@ -0,0 +1,11 @@
+class Ballon{
+	public static int[] totalBallons(){	
+		int[] noOfBallons={4,4,9,3,67,100};
+		return noOfBallons;
+	}
+	
+	public static float[] ballonPrice(){
+		float[] costOfBallons={2.76f,1.76f,3.26f,0.67f,0.75f,10.76f};
+		return costOfBallons;
+	}
+}
