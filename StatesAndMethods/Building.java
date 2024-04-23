be93b0db6a87b7acba67058fd8bb3d52c775402f
unknown <purushottamxworkz@gmail.com>
2024-04-23 14:59:38 +0530

    Added Building file

---
+++ StatesAndMethods/Building.java
@@ -0,0 +1,11 @@
+class Building{
+	public static int[] noOfFloors(){
+		int[] floorsOfBuilding={2,3,4,1};
+		return floorsOfBuilding;
+	}
+	
+	public static short[] sizeOfBuilding(){
+		short[] squareFeet={600,800,1200,900};
+		return squareFeet;
+	}
+}
