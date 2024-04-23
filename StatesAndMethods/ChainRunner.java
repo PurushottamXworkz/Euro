f2b0069ebf0e93df8263caade6fd42aec9c74732
unknown <purushottamxworkz@gmail.com>
2024-04-23 13:37:06 +0530

    Updated polymorphism

--- StatesAndMethods/ChainRunner.java
+++ StatesAndMethods/ChainRunner.java
@@ -2,6 +2,10 @@ class ChainRunner{
 	public static void main(String args[]){
 		Chain.lengthOfChain(24);
 		Chain.lengthOfChain(24,121.321f);
-		Chain.lengthOfChain(24,'L');
+		Chain.lengthOfChain(24,121.321f,'L');
+		Chain.lengthOfChain(24,121.321f,'L',"Arun Jewellary");
+		Chain.lengthOfChain(24,121.321f,'L',"Arun Jewellary",true);
+		Chain.lengthOfChain(24,121.321f,'L',"Arun Jewellary",true,78451243l);
+		Chain.lengthOfChain(24,121.321f,'L',"Arun Jewellary",true,78451243,8745967744l);
 	}
 }
