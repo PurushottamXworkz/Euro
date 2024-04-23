2c7b9085c07d065af7ede69e4e20029969223789
unknown <purushottamxworkz@gmail.com>
2024-04-23 13:37:50 +0530

    Updated polymorphism

--- StatesAndMethods/Shoe.java
+++ StatesAndMethods/Shoe.java
@@ -5,7 +5,19 @@ class Shoe{
 	public static void lengthOfShoe(int length,float mrp){
 		System.out.println("Length of Shoe : "+length +" and MRP of Shoe is : "+mrp);
 	}
-	public static void lengthOfShoe(int length,char typeOfshoe){
-		System.out.println("Length of Shoe : "+length + " and type of Shoe is : "+typeOfshoe);
+	public static void lengthOfShoe(int length,float mrp,char typeOfshoe){
+		System.out.println("Length of Shoe : "+length + " MRP of Shoe is : "+mrp +" and type of Shoe is : "+typeOfshoe);
+	}
+	public static void lengthOfShoe(int length,float mrp,char typeOfshoe,long contactForShoe){
+			System.out.println("Length of Shoe : "+length + " MRP of Shoe is : "+mrp +" type of Shoe is : "+typeOfshoe+" and contact for shoe : "+contactForShoe);
+	}
+	public static void lengthOfShoe(int length,float mrp,char typeOfshoe,long contactForShoe,double holeSalerate){
+			System.out.println("Length of Shoe : "+length + " MRP of Shoe is : "+mrp +" type of Shoe is : "+typeOfshoe+" contact for shoe : "+contactForShoe+" and holesale rate "+holeSalerate);
+	}
+	public static void lengthOfShoe(int length,float mrp,char typeOfshoe,long contactForShoe,double holeSalerate,String brandName){
+			System.out.println("Length of Shoe : "+length + " MRP of Shoe is : "+mrp +" type of Shoe is : "+typeOfshoe+" contact for shoe : "+contactForShoe+" holesale rate "+holeSalerate+" and brand name is :"+brandName);
+	}
+	public static void lengthOfShoe(int length,float mrp,char typeOfshoe,long contactForShoe,double holeSalerate,String brandName,boolean branIsAvail){
+			System.out.println("Length of Shoe : "+length + " MRP of Shoe is : "+mrp +" type of Shoe is : "+typeOfshoe+" contact for shoe : "+contactForShoe+" holesale rate "+holeSalerate+" brand name is :"+brandName+ " and  brand is available in the store : "+branIsAvail);
 	}
 }
