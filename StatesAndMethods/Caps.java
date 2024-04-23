32bfc3371e3282985fb6a4694929b1a7e78e61f9
unknown <purushottamxworkz@gmail.com>
2024-04-23 14:35:34 +0530

    Newly added file

---
+++ StatesAndMethods/Caps.java
@@ -0,0 +1,10 @@
+class Caps{
+	public static int[] totalCaps(){
+		int [] noOfCaps={4,3,12,5,34,65};
+		return noOfCaps;
+	}
+	public static String [] capName(){
+		String [] nameOfCap={"Adidas","logo","hp","leetCode","codeForces","US-Navy"};
+		return nameOfCap;
+	}
+}
