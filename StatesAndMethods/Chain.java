9c82bab53d6971c3dd06a6fdbd49e041c9f34c89
unknown <purushottamxworkz@gmail.com>
2024-04-23 13:33:00 +0530

    Updated polymorphism

--- StatesAndMethods/Chain.java
+++ StatesAndMethods/Chain.java
@@ -5,7 +5,19 @@ class Chain{
 	public static void lengthOfChain(int length,float mrp){
 		System.out.println("Length of chain : "+length +" and MRP of chain is : "+mrp);
 	}
-	public static void lengthOfChain(int length,char typeOfchain){
-		System.out.println("Length of chain : "+length + " and type of chain is : "+typeOfchain);
+	public static void lengthOfChain(int length,float mrp,char typeOfchain){
+		System.out.println("Length of chain is : "+length + " MRP of chain is : "+mrp + " and Type of Chain is "+typeOfchain);
+	}
+	public static void lengthOfChain(int length,float mrp,char typeOfchain,String nameOfShop){
+		System.out.println("Length of chain is : "+length + " MRP of chain is : "+mrp + " Type of Chain is "+typeOfchain + " and the name of Shop is :"+nameOfShop);
+	}
+	public static void lengthOfChain(int length,float mrp,char typeOfchain,String nameOfShop,boolean patternOfChain){
+			System.out.println("Length of chain is : "+length + " MRP of chain is : "+mrp + " Type of Chain is "+typeOfchain + " the name of Shop is :"+nameOfShop + " and the pattern of chain is present : "+patternOfChain);
+	}
+	public static void lengthOfChain(int length,float mrp,char typeOfchain,String nameOfShop,boolean patternOfChain,long rateOfGoldPkg){
+			System.out.println("Length of chain is : "+length + " MRP of chain is : "+mrp + " Type of Chain is "+typeOfchain + " the name of Shop is :"+nameOfShop + " the pattern of chain is present : "+patternOfChain +" and rate of gold per kg is : "+rateOfGoldPkg);
+	}
+	public static void lengthOfChain(int length,float mrp,char typeOfchain,String nameOfShop,boolean patternOfChain,long contactForGold,double rateOfGoldPkg){
+			System.out.println("Length of chain is : "+length + " MRP of chain is : "+mrp + " Type of Chain is "+typeOfchain + " the name of Shop is :"+nameOfShop + " the pattern of chain is present : "+patternOfChain +" rate of gold per kg is : "+contactForGold + " contact for gold " +rateOfGoldPkg);
 	}
 }
