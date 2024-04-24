4f59f63ce72691c1d4cedaf52e4d0cc4951e1da5
unknown <purushottamxworkz@gmail.com>
2024-04-24 15:43:12 +0530

    Adding ship file

---
+++ StatesAndMethods/Ship.java
@@ -0,0 +1,10 @@
+class Ship{
+	public static int[] noOfShip(int [] numOfShips){
+		return numOfShips;
+	}
+	
+	public static String[] membersName(){
+		String[] namesOfThemembersofTheShip={"Chandler","Scott","Stacks","Adam","Erish"};
+		return namesOfThemembersofTheShip;
+	}
+}
