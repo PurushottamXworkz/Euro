3eb37e74fa6bf390e12448bab8614de076318c34
unknown <purushottamxworkz@gmail.com>
2024-04-24 15:42:06 +0530

    Using return keyword retriving values

---
+++ StatesAndMethods/Mole.java
@@ -0,0 +1,10 @@
+class Mole{
+	public static String[] comnmanderNameOfEachCamp(){
+		String[] commandoNameVaries={"Dhruv","Prahlad","Abhinav","Vinod","Vikram"};
+		return commandoNameVaries;
+	}
+	
+	public static int[] noOfCamps(int [] totalCamps){
+		return totalCamps;
+	}
+}
