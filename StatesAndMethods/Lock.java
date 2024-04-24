5c98ed3a420f01f44f1fc39e1a11654f0097f59f
unknown <purushottamxworkz@gmail.com>
2024-04-24 15:41:08 +0530

    Using return keyword retriving values

---
+++ StatesAndMethods/Lock.java
@@ -0,0 +1,10 @@
+class Lock{
+	public static int[] noOfLocks(int [] locknumber){
+		return locknumber;
+	}
+	
+	public static String[] ownerName(){
+		String[] ownersNameVaries={"Raj","Pritvi","Shashi","Dileep","Manoj"};
+		return ownersNameVaries;
+	}
+}
