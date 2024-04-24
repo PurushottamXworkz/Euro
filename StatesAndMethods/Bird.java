3229d6aeacaa4269f9f2ed87fd2c0ccd53c58723
unknown <purushottamxworkz@gmail.com>
2024-04-24 15:39:10 +0530

    Using return keyword retriving values

---
+++ StatesAndMethods/Bird.java
@@ -0,0 +1,10 @@
+class Bird{
+	public static String [] birdName(){
+	String [] nameOfBirds={"Ostrich","Eagle","HornBill","Cranes"};
+	return nameOfBirds;
+	}
+	
+	public static int[] birdAge(int[] age){
+		return age;
+	}
+}
