164852cfe83ac4ef8a3b3d7682a161591b56bef8
unknown <purushottamxworkz@gmail.com>
2024-04-27 16:00:00 +0530

    Constructor file

---
+++ StatesAndMethods/Rods.java
@@ -0,0 +1,21 @@
+class Rods{
+	int rodsNumber=89;
+	float rodsThickness=4.22f;
+	double allRodsThickness=89.2415;
+	long contactForRods=8762772285l;
+	char typeOfRods='L';
+	boolean rodsPresent=true;
+	String nameOfrods="Tikare";
+	
+	public void addCustomer(){
+		System.out.println("Adding customer to rods store");
+	}
+	
+	public void updateCutomer(){
+		System.out.println("Updating the customer info ");
+	}
+	
+	public void deleteCustomer(){
+		System.out.println("Removing the customer ");
+	}
+}
