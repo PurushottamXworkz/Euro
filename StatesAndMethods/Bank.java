cc7f3251bb71a520d063ae22db15ac0a8380c7ca
unknown <purushottamxworkz@gmail.com>
2024-04-27 15:46:43 +0530

    Constructor file

---
+++ StatesAndMethods/Bank.java
@@ -0,0 +1,18 @@
+class Bank{
+	int noOfCustomers=457;
+	float trasactionAmt=120.332f;
+	double totalValue=213452.982132;
+	char typeOfBank='C';
+	String nameOfBank="Canara";
+	long contactNum=8762772285l;
+	boolean isBankOpen=true;
+	public void addCustomer(){
+		System.out.println("Add the Customer ");
+	}
+	public void updateCustomer(){
+		System.out.println("Update info Customer ");
+	}
+	public void deleteCustomer(){
+		System.out.println("Delete Customer ");
+	}
+}
