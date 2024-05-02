7e0286affc188f92c14f9a6f756fc01ed1dfab6c
unknown <purushottamxworkz@gmail.com>
2024-05-02 12:59:42 +0530

    setter and getter method

---
+++ StatesAndMethods/BoardRunner.java
@@ -0,0 +1,14 @@
+class BoardRunner{
+	
+	public static void main(String []args){
+		
+		Board board=new Board();
+		board.setCostOfBoard(10000);
+		board.setSizeOfBoard(100.219676d);
+		board.setBoardColor("White");
+		board.getCostOfBoard();
+		board.getSizeOfBoard();
+		board.getBoardColor();
+		
+	}
+}
