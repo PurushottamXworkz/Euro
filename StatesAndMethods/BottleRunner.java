9d122de605fe3b29bb9f171c3e2fbbafe1261efb
unknown <purushottamxworkz@gmail.com>
2024-05-02 13:08:19 +0530

    setter and getter method

---
+++ StatesAndMethods/BottleRunner.java
@@ -0,0 +1,8 @@
+class BottleRunner{
+	public static void main(String args[]){
+		Bottle bottle=new Bottle();
+		bottle.setPriceOfBottle(987);
+		bottle.getPriceOfBottle();
+		bottle.getBrandOfBottle();
+	}
+}
