33bc65b1018a2be55b399e659a978965a8cf1755
unknown <purushottamxworkz@gmail.com>
2024-05-02 13:10:09 +0530

    setter and getter

---
+++ StatesAndMethods/LaptopRunner.java
@@ -0,0 +1,12 @@
+class LaptopRunner{
+	
+	public static void main(String []args){
+	    Laptop laptop=new Laptop();
+		laptop.setPrice(30000);
+		laptop.setRom(512);
+		laptop.setCompanyName("DELL");
+		laptop.getPrice();
+		laptop.getRom();
+		laptop.getCompanyName();		
+	}
+}
