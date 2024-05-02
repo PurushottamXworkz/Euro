978f7057907dd92ae3be83ad460234c040dbfd3b
unknown <purushottamxworkz@gmail.com>
2024-05-02 13:11:43 +0530

    setter and getter method

---
+++ StatesAndMethods/Pen.java
@@ -0,0 +1,11 @@
+class Pen{
+
+// 11 methods from parent class
+// constructor default constructor
+// its a special methods
+
+//instance-> non-static 
+ public  String penName;
+ // default constructor
+
+}
---
+++ StatesAndMethods/PenRunner.java
@@ -0,0 +1,12 @@
+class PenRunner{
+
+
+  public static void main(String args[]){
+  
+   Pen pen= new Pen();
+   System.out.println(pen);
+   Pen pen1= new Pen();
+   System.out.println(pen1);
+ 
+  }
+}
