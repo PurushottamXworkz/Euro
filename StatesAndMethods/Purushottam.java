6021bf34380ec21a77c238deacb1b89bc71088bc
unknown <purushottamxworkz@gmail.com>
2024-05-02 13:13:11 +0530

    For loop

---
+++ StatesAndMethods/Purushottam.java
@@ -0,0 +1,8 @@
+class Purushottam{
+	 static public void main(String args[]){
+		System.out.println("This is Purushottam");
+		for(int i=0;i<=args.length-1;i++){
+			System.out.println("values "+args[i]);
+		}
+	}
+}
