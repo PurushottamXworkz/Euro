e2cc527dddb11e0f4df6ac5b9ba85c17345d5667
unknown <purushottamxworkz@gmail.com>
2024-05-02 13:15:58 +0530

    setter and getter method

---
+++ StatesAndMethods/TrainsRunner.java
@@ -0,0 +1,13 @@
+class TrainsRunner{
+	
+	public static void main(String []args){
+		
+		Trains train=new Trains();
+		train.setNoOfCoach(30);
+		train.setNameOfTheTrain("Shatabdi");
+		train.setTypeOfTrain('E');
+		train.getNoOfCoach();
+		train.getNameOfTheTrain();
+		train.getTypeOfTrain();	
+	}
+}
