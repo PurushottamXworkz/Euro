d5a87f243401414896cb5062ae6e888e8c038797
unknown <purushottamxworkz@gmail.com>
2024-05-07 12:21:50 +0530

    No arguement constructor and methods

---
+++ StatesAndMethods/Child.java
@@ -0,0 +1,19 @@
+class Child{
+	int noOfChilds=2;
+	String nameOfChild="Kriti";
+	boolean isChildNoughty=true;
+	public Child(){
+		System.out.println("This is no arguement constructor");
+	}
+	public void name(String childName){
+		System.out.println("The name of child is : "+this.nameOfChild);
+		nameOfChild=childName;
+		System.out.println("The name of child is replaced by : "+this.nameOfChild);
+	}
+	public void active(boolean chlidActive){
+		System.out.println("The child is active ? : "+this.isChildNoughty);
+		isChildNoughty=chlidActive;
+		System.out.println("The child is active ? : "+this.isChildNoughty);
+	}
+	
+}
