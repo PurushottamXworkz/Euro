f4e9b57d02c07784120a3da7f9adb7a925442524
unknown <purushottamxworkz@gmail.com>
2024-05-07 12:26:07 +0530

    No arguement constructor and methods

---
+++ StatesAndMethods/Coil.java
@@ -0,0 +1,18 @@
+class Coil{
+	int numberOfCoils=410;
+	String brandName="Aspire";
+	boolean brandIsSame=true;
+	public Coil(){
+		System.out.println("This is no arguement constructor");
+	}
+	public void nameOfBrand(String brand){
+		System.out.println("The brand name is : "+this.brandName);
+		brandName=brand;
+		System.out.println("The brand name is : "+this.brandName);
+	}
+	public void availability(boolean brandIsPresent){
+		System.out.println("Is the is present ? : "+this.brandIsSame);
+		brandIsSame=brandIsPresent;
+		System.out.println("Is the is present ? : "+this.brandIsSame);
+	}
+}
