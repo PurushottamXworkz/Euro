405fd17092fbd4e40c0a32ea7c16f0bea984d111
unknown <purushottamxworkz@gmail.com>
2024-05-07 12:28:09 +0530

    Class Activity

---
+++ StatesAndMethods/HomeRunner.java
@@ -0,0 +1,8 @@
+class HomeRunner{
+	public static void main(String args[]){
+		Home home=new Home();
+		home.homeInfo(24);
+		home.homeInfo(24,"Pattanashetty");
+		home.homeInfo(24,"Pattanashetty","Father");
+	}
+}
