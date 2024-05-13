0a41e7e01601976315d615bc801bc64f6462d68b
unknown <purushottamxworkz@gmail.com>
2024-05-13 13:08:11 +0530

    Constructor

---
+++ StatesAndMethods/Multiplex.java
@@ -0,0 +1,10 @@
+class Multiplex{
+	public Multiplex(){
+		System.out.println("This is Multiplex no arguement constructor");
+		nameOfTheMovie();
+		Movie1 movie=new Movie1();
+	}
+	public static void nameOfTheMovie(){
+		System.out.println("Movie name is UI");
+	}
+}
