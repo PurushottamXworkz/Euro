43ba035840bd7dadcc5b07ea036257e79a0997c9
unknown <purushottamxworkz@gmail.com>
2024-05-14 16:54:21 +0530

    Constructor with user defined datatype

---
+++ StatesAndMethods/Securities.java
@@ -0,0 +1,8 @@
+class Securities{
+	public int noOfSecuries;
+	public String nameOfPrimeMinister;
+	public Securities(String nameOfPrimeMinister,int noOfSecuries){
+		this.noOfSecuries=noOfSecuries;
+		this.nameOfPrimeMinister=nameOfPrimeMinister;
+	}  
+}
