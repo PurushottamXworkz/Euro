00712ecd20344f698ffff364d5c276f747574a79
unknown <purushottamxworkz@gmail.com>
2024-05-14 16:53:59 +0530

    Constructor with user defined datatype

---
+++ StatesAndMethods/PrimeMinisterRunner.java
@@ -0,0 +1,8 @@
+class PrimeMinisterRunner{
+	public static void main(String args[]){
+		PrimeMinister pm=new PrimeMinister("Narendra Modi",100);
+		Securities securities=new Securities();
+		securities.pmDetails(125);
+		
+	}
+}
