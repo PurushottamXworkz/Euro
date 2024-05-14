c7997fcf3117e89617cd440a520fb94e0c0a184b
unknown <purushottamxworkz@gmail.com>
2024-05-14 20:17:50 +0530

    One to one association using constructor

---
+++ StatesAndMethods/Car1Runner.java
@@ -0,0 +1,8 @@
+class Car1Runner{
+	public static void main(String args[]){
+		Car1 car1 = new Car1("Abhi","RR Nagar",7841541420l);
+		Details detail=new Details();
+		detail.display("basic","Tata",car1);
+		
+	}
+}
