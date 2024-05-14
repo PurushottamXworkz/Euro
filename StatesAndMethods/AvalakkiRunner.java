944e1092b7d5651f46024f3712c77c5005d257bb
unknown <purushottamxworkz@gmail.com>
2024-05-14 16:51:00 +0530

    Constructor with user defined datatype

---
+++ StatesAndMethods/AvalakkiRunner.java
@@ -0,0 +1,7 @@
+class AvalakkiRunner{
+	public static void main(String []args){
+		Oggarni oggarni=new Oggarni(125,"Chummari");
+		Avalakki avalakki=new Avalakki();
+		avalakki.avalakkiDetails("Soosala",oggarni);
+	}
+}
