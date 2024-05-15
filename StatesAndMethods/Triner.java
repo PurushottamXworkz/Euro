cc39a5c29572e06bfe809fb1c1fc77a86f01d817
unknown <purushottamxworkz@gmail.com>
2024-05-15 14:49:54 +0530

    Association constructor

---
+++ StatesAndMethods/Triner.java
@@ -0,0 +1,14 @@
+class Triner{
+	public String nameOfTriner;
+	public String address;
+	public long contact;
+	
+	public Triner(String nameOfTriner,String address,long contact){
+		System.out.println("-------------------------------------");
+		System.out.println("Name of Triner : "+nameOfTriner+" Address : "+address+" Contact : "+contact);
+		this.nameOfTriner=nameOfTriner;
+		this.address=address;
+		this.contact=contact;
+		System.out.println("-------------------------------------");
+	}
+}
