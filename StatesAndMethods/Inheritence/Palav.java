c28e97aff412e81c602e691455b0ca2bddf66efc
unknown <purushottamxworkz@gmail.com>
2024-05-22 11:01:37 +0530

    instance variable and use of super keyword

---
+++ StatesAndMethods/Inheritence/Palav.java
@@ -0,0 +1,8 @@
+class Palav{
+	public int noOfIncredients=10;
+	public int noOfMasala=1;
+	public int noOfPeas=200;
+	public void typesOfPalav(){
+		System.out.println("This is Palav parent class");
+	}
+}
