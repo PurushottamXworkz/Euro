1b302a1c42723afd57fd87931706cd6e6d470b60
unknown <purushottamxworkz@gmail.com>
2024-05-22 10:57:36 +0530

    instance variable and use of super keyword

---
+++ StatesAndMethods/Inheritence/GoogleRunner.java
@@ -0,0 +1,11 @@
+class GoogleRunner{
+	public static void main(String args[]){
+	ProductsOfGoogle productOfgoogle=new ProductsOfGoogle();
+	productOfgoogle.products();
+	productOfgoogle.googleProducts();
+	productOfgoogle.productName();
+	productOfgoogle.owner();
+	productOfgoogle.productsInNumber();
+	}
+	
+}
