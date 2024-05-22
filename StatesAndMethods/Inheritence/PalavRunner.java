74cda22c9c62ca16678bf393c29c632ea50d8ba0
unknown <purushottamxworkz@gmail.com>
2024-05-22 11:04:38 +0530

    instance variable and use of super keyword

---
+++ StatesAndMethods/Inheritence/PalavRunner.java
@@ -0,0 +1,10 @@
+class PalavRunner{
+	public static void main(String args[]){
+		VegPalav vegpalav = new VegPalav();
+		vegpalav.typesOfPalav();
+		vegpalav.breadPalav();
+		vegpalav.incredients();
+		vegpalav.masala();
+		vegpalav.peas();		
+	}
+}
