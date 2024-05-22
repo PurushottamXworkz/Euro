49b753958a1a6d0fe2710367c8ce6a1cb47af8bd
unknown <purushottamxworkz@gmail.com>
2024-05-22 11:11:49 +0530

    instance variable and use of super keyword

---
+++ StatesAndMethods/Inheritence/RottiRunner.java
@@ -0,0 +1,10 @@
+class RottiRunner{
+	public static void main(String args[]){
+		Tandoori tandoori=new Tandoori();
+		tandoori.typesOfRoti();
+		tandoori.rottiType();
+		tandoori.rotiName();
+		tandoori.rotisInNumber();
+		tandoori.amount();
+	}
+}
