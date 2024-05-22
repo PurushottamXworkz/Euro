347aa49e7c5cdc1b6973e5f92109e624ed0e1c28
unknown <purushottamxworkz@gmail.com>
2024-05-22 10:56:37 +0530

    instance variable and use of super keyword

---
+++ StatesAndMethods/Inheritence/Google.java
@@ -0,0 +1,8 @@
+class Google{
+	public String nameOfProduct="GooglepixelAI";
+	public String manufacturer="Google";
+	public int noOfproducts=10;
+	public void products(){
+		System.out.println("This is parent class of Google");
+	}
+}
