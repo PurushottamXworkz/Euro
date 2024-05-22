ecfbfd9ea1a8327638b7cf62c75172d634d94682
unknown <purushottamxworkz@gmail.com>
2024-05-22 11:16:30 +0530

    instance variable and use of super keyword

---
+++ StatesAndMethods/Inheritence/Microsoft.java
@@ -0,0 +1,8 @@
+class Microsoft{
+	public String nameOfProduct="Outlook";
+	public String manufacturer="Microsoft";
+	public int noOfproducts=15;
+	public void products(){
+		System.out.println("This is parent class of Microsoft");
+	}
+}
