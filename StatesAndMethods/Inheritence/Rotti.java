e34f0e303612c94e5328926576fd7b85cc8c4136
unknown <purushottamxworkz@gmail.com>
2024-05-22 11:05:56 +0530

    instance variable and use of super keyword

---
+++ StatesAndMethods/Inheritence/Rotti.java
@@ -0,0 +1,8 @@
+class Rotti{
+	public String nameOfRoti="Tandoori";
+	public int noOfRotis=5;
+	public int amountOfRoti=50;
+	public void typesOfRoti(){
+		System.out.println("This is parent Roti class");
+	}
+}
