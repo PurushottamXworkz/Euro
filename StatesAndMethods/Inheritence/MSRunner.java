610d71b915638536b59271422b7f981a236bf065
unknown <purushottamxworkz@gmail.com>
2024-05-22 11:12:41 +0530

    instance variable and use of super keyword

---
+++ StatesAndMethods/Inheritence/MSRunner.java
@@ -0,0 +1,12 @@
+class MSRunner{
+	public static void main(String args[]){
+	ProductsOfMS productOfms=new ProductsOfMS();
+	productOfms.products();
+	productOfms.msProducts();
+	productOfms.productName();
+	productOfms.owner();
+	productOfms.productsInNumber();
+	
+	}
+	
+}
