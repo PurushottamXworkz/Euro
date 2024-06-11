9100483ef462f04ae72186df5f8cba39f8faa324
unknown <prashantpattanashetti5@gmail.com>
2024-06-11 12:51:29 +0530

    coding problems

---
+++ codingproblems/src/reverse/ReverseString.java
@@ -0,0 +1,11 @@
+package reverse;
+
+public class ReverseString {
+	public static void main(String args[]) {
+		String name="PURUSHOTTAM";
+		System.out.println(name.length());
+			for(int i=name.length();i>=0;i--) {
+				System.out.println();
+			}
+	}
+}
